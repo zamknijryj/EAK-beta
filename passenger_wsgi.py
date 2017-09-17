ac99bb8d25d7ea6bd9d7621b5487fa2d2287d84b
zamknijryj <zamknijryjx.x@tutanota.com>
2017-09-17 12:09:58 +0200

    EAK

---
+++ passenger_wsgi.py
@@ -0,0 +1,7 @@
+import sys, os
+
+sys.path.append(os.getcwd())
+os.environ['DJANGO_SETTINGS_MODULE'] = "nazwa_aplikacji.settings"  # zmienić 'nazwa_aplikacji' na
+                                                                   # nazwę projektu Django                                                                      
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
