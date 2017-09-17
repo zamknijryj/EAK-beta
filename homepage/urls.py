c7b97aefdb4cafa84894f87fa9eb34302d0f5527
zamknijryj <zamknijryjx.x@tutanota.com>
2017-09-17 11:47:41 +0200

    EAK - abc

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,8 +1,13 @@
 from django.contrib.auth.urls import url
-from . import views
+from .views import (
+    Homepage,
+    RejstrZolnierzy,
+    AbcEak
+)
 
 urlpatterns = [
-    url(r'^$', views.homepage, name='homepage'),
-    url(r'^rejestr-zolnierzy', views.rejestr_zolnierzy, name='rejestr-zolnierzy'),
+    url(r'^$', Homepage.as_view(), name='homepage'),
+    url(r'^rejestr-zolnierzy/$', RejstrZolnierzy.as_view(), name='rejestr-zolnierzy'),
+    url(r'^abc/$', AbcEak.as_view(), name='abc-eak')
 
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
-def homepage(request):
-    return render(request, 'homepage/homepage.html')
-
-def rejestr_zolnierzy(request):
-    return render(request, 'homepage/rejestr.html')
+class Homepage(TemplateView):
+    template_name = 'homepage/homepage.html'
 
 
+class RejstrZolnierzy(TemplateView):
+    template_name = 'homepage/rejestr.html'
+
+
+class AbcEak(TemplateView):
+    template_name = 'homepage/abc.html'
