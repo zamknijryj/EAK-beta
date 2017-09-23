427dd04a0c14b67e509c108d00f76e6270d84dc4
zamknijryj <zamknijryjx.x@tutanota.com>
2017-09-23 18:04:18 +0200

    EAK

--- aktualnosci/models.py
+++ aktualnosci/models.py
@@ -19,19 +19,18 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length = 10,
+    status = models.CharField(max_length=10,
                               choices=STATUS_CHOICES,
                               default='draft')
 
     class Meta:
         ordering = ['-publish']
 
-
     def get_absolute_url(self):
         return reverse('test', args=[self.publish.year,
-                                    self.publish.strftime('%m'),
-                                    self.publish.strftime('%d'),
-                                    self.slug])
+                                     self.publish.strftime('%m'),
+                                     self.publish.strftime('%d'),
+                                     self.slug])
 
     def __str__(self):
         return self.title
